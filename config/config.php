deb11175828f09d8d4c548bc3df5cbd1f318037f
candradwicahyo <candradwicahyo18@gmail.com>
2021-01-21 03:25:12 +0700

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// config 
+define('hostname', 'localhost');
+define('username', 'root');
+define('password', '');
+define('database', 'pendataan_siswa');
+define('url', 'http://localhost:8000/halaman/pendataan_siswa/');
+define('ekstension', ['html' => '.html', 'css' => '.css', 'js' => '.js', 'php' => '.php']);
+define('author', 'candra dwi cahyo');
+define('gender', ['laki-laki', 'perempuan', 'rahasia']);
+define('kelas', ['VIII A', 'VIII B', 'VIII C', 'VIII D']);
+define('jurusan', ['rpl', 'tkj', 'akuntansi', 'ekonomi', 'robotik']);
